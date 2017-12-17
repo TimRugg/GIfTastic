4b1667ecd44aa6d995b3af8500aca4443cb534b1
timster2938 <ruggt001@icloud.com>
2017-12-17 13:01:24 -0500

    step one: array of topics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,21 @@
-// example for image directory
-// document.getElementById("gameImage").setAttribute("src", "./assets/images/hangmanWIN.jpg");
+//create variables
+var topics = ["football","soccer","lacrosse","hockey","tennis"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//giphy api key = OAJw0fyFvlk0O3cfeAOeBttFwgm67zS9
+//javascript, jQuery
+// var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=OAJw0fyFvlk0O3cfeAOeBttFwgm67zS9&limit=5");
+// xhr.done(function(data) { console.log("success got data", data); });
+// https://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=OAJw0fyFvlk0O3cfeAOeBttFwgm67zS9&limit=5&rating=g;
