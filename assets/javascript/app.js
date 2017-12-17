2c1b34f5c1e795dda89bb2bcafe96769487b742a
timster2938 <ruggt001@icloud.com>
2017-12-17 14:01:23 -0500

    step two: add buttons to page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,18 @@
 //create variables
 var topics = ["football","soccer","lacrosse","hockey","tennis"];
 
-
-
-
-
-
+//loop through the array displaying a button for each element in the topics array
+for (var i=0; i<topics.length; i++) {
+	console.log("index: " + i + " number of topics:" + topics.length + " topics:" + topics[i])
+	//the following works too but the previous would be easier to edit
+	// $('#displayButtons').append("<button class='topicButton' value=" + i + ">" + topics[i] + "</button>;");
+	//create a variable and build the html for each button using jquery
+	var buildButton = $("<button>");			//jquery will also insert the end tag
+	buildButton.addClass("topicButton");		//insert a class of topicButton into each button
+	buildButton.attr("topicValue", topics[i]); 	//insert a new attr named topicValue and set to current topic
+	buildButton.text(topics[i]);				//text is the displayed lable on the button
+	$("#displayButtons").append(buildButton);	//add button to display or to end of displayed buttons
+}
 
 
 
